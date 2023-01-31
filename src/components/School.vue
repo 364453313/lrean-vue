7c7ca55174005f5361289f9f926478f50d909902
zn yang <364453313@qq.com>
2023-01-31 16:53:41 +0800

    初建测试小项目

---
+++ src/components/School.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>{{school}}</h3>
+    <button @click="showName">展示</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data(){
+        return {
+            school:'社会大学'
+        }
+    },
+    methods: {
+        showName(){
+            alert(this.school)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{student}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+        return {
+            student:'杨岳伟'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
